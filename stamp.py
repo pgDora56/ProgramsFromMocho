7046082b74869606122276cfb9f65d31a9a8acef
Dora F <doradora.prog@gmail.com>
2022-05-20 00:57:39 +0900

    Commit from auto-commit

--- stamp.py
+++ stamp.py
@@ -1,3 +1,4 @@
+# Created by Dora at 2022-05-20 00:57:31
 import random
-r = random.randrange(325858925)
+r = random.randrange(499033861)
 print(f"https://stickershop.line-scdn.net/stickershop/v1/sticker/{r}/android/sticker.png")
